f3eda78721bc5e1e4550a5c23096a995a23f378c
Alexey Pravdin <a.pravdin@itq-group.com>
2025-12-01 17:18:53 +0300

    initial project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import {reactive} from "vue";
+
+export interface Form {
+  input: string;
+  input2: string;
+  input3: string;
+}
+
+const formData: Form = reactive({
+  input: '',
+  input2: '',
+  input3: ''
+})
+</script>
+
+<template>
+  <div>
+    <p>{{ formData.input }}</p>
+    <p>{{ formData.input2 }}</p>
+    <p>{{ formData.input3 }}</p>
+  </div>
+  <HelloWorld v-model="formData"/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type {Form} from "../App.vue";
+
+const formData = defineModel<Form>({required: true})
+</script>
+
+<template>
+  <input type="text" v-model="formData.input"/>
+  <input type="text" v-model="formData.input2"/>
+  <input type="text" v-model="formData.input3"/>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
